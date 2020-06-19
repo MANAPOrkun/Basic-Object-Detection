84832936dcba41159898ab4f395ce394dfb2d7f8
Orkun Manap <manaporkun@gmail.com>
2020-06-19 14:38:32 +0300

    update

--- 06-Feature-Matching/feature_matching.py
+++ 06-Feature-Matching/feature_matching.py
@@ -61,5 +61,36 @@ def sift(product, products):
 
     return sift_matches 
 
-display(sift(product, products))
+def flann(product, products):
+
+    # Initiate SIFT detector
+    sift = cv2.xfeatures2d.SIFT_create()
+
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(product,None)
+    kp2, des2 = sift.detectAndCompute(products,None)
+
+    # FLANN parameters
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks=50)  
+
+    flann = cv2.FlannBasedMatcher(index_params,search_params)
+
+    matches = flann.knnMatch(des1,des2,k=2)
+
+    good = []
+
+    # ratio test
+    for i,(match1,match2) in enumerate(matches):
+        if match1.distance < 0.7*match2.distance:
+        
+            good.append([match1])
+
+
+    flann_matches = cv2.drawMatchesKnn(product,kp1,products,kp2,good,None,flags=0)
+
+    return flann_matches
+
+display(flann(product, products))
 
